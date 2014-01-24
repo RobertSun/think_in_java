f7f88d09190e52ebcf123cde3e92a2fcdb69504b
RobertSun <sundechun@gmail.com>
2014-01-24 15:38:24 +0800

    Class_NullObject

---
+++ src/com/test/typeinfo/Operation.java
@@ -0,0 +1,22 @@
+/**
+ * Filename:    Operation.java
+ * Description:
+ * Copyright:   Copyright (c)2010
+ * Company:     英睿（大连）信息有限公司
+ * @author:     Robert Sun
+ * @version:    1.0
+ * Create at:   2014年1月24日 下午3:03:05
+ * 
+ * Modification History:
+ * Date         Author        Version       Description
+ * ------------------------------------------------------------------  
+ * 2014年1月24日     Robert Sun     1.0         1.0 Version
+ */
+package com.test.typeinfo;
+
+//: typeinfo/Operation.java 
+
+public interface Operation { 
+	String description(); 
+	void command(); 
+} ///:~ 
---
+++ src/com/test/util/Null.java
@@ -0,0 +1,4 @@
+package com.test.util;
+
+//: net/mindview/util/Null.java 
+public interface Null {} ///:~ 
