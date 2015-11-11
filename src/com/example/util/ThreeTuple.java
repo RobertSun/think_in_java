e5ff4ee4deef7ed2f2cb75c1c6a5e429b05e3791
RobertSun <sundechun@gmail.com>
2015-11-11 11:42:29 +0800

    Updated to "Generics-RandomList".

---
+++ src/com/example/util/ThreeTuple.java
@@ -0,0 +1,18 @@
+package com.example.util;
+
+/**
+ * Created by Devin on 2015/11/6.
+ */
+public class ThreeTuple<A,B,C> extends TwoTuple<A,B> {
+  public final C third;
+
+  public ThreeTuple(A a, B b, C c) {
+    super(a, b);
+    third = c;
+  }
+
+  public String toString() {
+    return "(" + first + ", " + second + ", " + third +")";
+  }
+} ///:~
+
