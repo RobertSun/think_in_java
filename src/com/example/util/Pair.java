cc017b06a1f16677058f419e1da70f91071437cc
RobertSun <sundechun@gmail.com>
2016-01-10 22:45:48 +0800

    Updated to "Unsupported operation".

---
+++ src/com/example/util/Pair.java
@@ -0,0 +1,11 @@
+package com.example.util;
+
+//: net/mindview/util/Pair.java
+public class Pair<K,V> {
+    public final K key;
+    public final V value;
+    public Pair(K k, V v) {
+        key = k;
+        value = v;
+    }
+} ///:~
